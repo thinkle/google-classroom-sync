1c69d192bab2c05801d9029a827085aa6b0fccfe
Tom Hinkle <tmhinkle@gmail.com>
2025-03-05 10:33:00 -0500

    add hooks to initialize values from remove values (seems like this wasn't happening elsewhere???)

--- src/svelte/store.ts
+++ src/svelte/store.ts
@@ -1,4 +1,4 @@
-import { derived, Writable, writable } from "svelte/store";
+import { derived, get, Writable, writable } from "svelte/store";
 import { GoogleAppsScript } from "./gasApi";
 import type { Readable } from "svelte/store";
 
@@ -6,14 +6,15 @@ export let lineItemStore: Writable<{ [key: string]: any[] }> = writable({});
 
 function createPersistentStoreWithHooks<T>(
   key: string,
-  startValue: T
+  startValue: T,
+  remoteInitializer?: () => Promise<T> | null
 ): Readable<T> & {
   setKey: (key: string, value: any) => void;
   addHook: (hook: (key: string, value: any) => void) => void;
   removeHook: (hook: (key: string, value: any) => void) => void;
 } {
   const keyHooks: Array<(key: string, value: any) => void> = [];
-  let store = createPersistentStore(key, startValue);
+  let store = createPersistentStore(key, startValue, remoteInitializer);
   return {
     subscribe: store.subscribe,
     setKey: (key: string, value: any) => {
@@ -36,12 +37,30 @@ function createPersistentStoreWithHooks<T>(
 
 function createPersistentStore<T>(
   key: string,
-  startValue: T
+  startValue: T,
+  remoteInitializer?: () => Promise<T> | null
 ): Writable<T> & {} {
   const storedValue = localStorage.getItem(key);
   const initialValue = storedValue ? JSON.parse(storedValue) : startValue;
 
   const store = writable<T>(initialValue);
+  if (remoteInitializer) {
+    remoteInitializer().then((value) => {
+      let $store = get(store);
+      let updates: { [key: string]: any } = {};
+      for (let key in value) {
+        let remoteValue = value[key];
+        if (JSON.stringify(remoteValue) !== $store[key]) {
+          updates[key] = remoteValue;
+        }
+      }
+      if (Object.keys(updates).length > 0) {
+        store.update((current) => {
+          return { ...current, ...updates };
+        });
+      }
+    });
+  }
 
   store.subscribe((value) => {
     localStorage.setItem(key, JSON.stringify(value));
@@ -74,11 +93,19 @@ export const studentLookup = createPersistentStore<{ [key: string]: any }>(
 
 export const assignmentMap = createPersistentStoreWithHooks<KeyMap>(
   "assignmentMap",
-  {}
+  {},
+  async () => {
+    let existingConnections = await GoogleAppsScript.getAssessmentConnections();
+    return existingConnections;
+  }
 );
 export const rubricAssignments = createPersistentStoreWithHooks<{
   [key: string]: { [key: string]: string };
-}>("rubricAssignmentStore", {});
+}>("rubricAssignmentStore", {}, async () => {
+  let existingConnections =
+    await GoogleAppsScript.getRubricAssessmentConnections();
+  return existingConnections;
+});
 
 rubricAssignments.addHook((googleAssignmentKey, criterionAssignmentMap) => {
   for (let criterionKey in criterionAssignmentMap) {
