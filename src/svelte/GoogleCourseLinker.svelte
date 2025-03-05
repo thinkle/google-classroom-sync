44600b779d688a1e7346c8324ff5f0ce01686ad3
Tom Hinkle <tmhinkle@gmail.com>
2025-03-05 10:30:33 -0500

    ++callback when done

--- src/svelte/GoogleCourseLinker.svelte
+++ src/svelte/GoogleCourseLinker.svelte
@@ -1,36 +1,39 @@
 <script>
-  import { Button, MenuList } from "contain-css-svelte";  
-   import { GoogleAppsScript } from "./gasApi";
+  import { Button, MenuList } from "contain-css-svelte";
+  import { GoogleAppsScript } from "./gasApi";
   export let aspenCourse;
+  export const onLink = (googleId, aspenId) => {};
   import { courseMap } from "./store";
   let courses = [];
-  async function loadCourses () {
-    console.log('Load courses...')
-    courses = await GoogleAppsScript.fetchGoogleCourses(); 
-    console.log('Got courses',courses);
+  async function loadCourses() {
+    console.log("Load courses...");
+    courses = await GoogleAppsScript.fetchGoogleCourses();
+    console.log("Got courses", courses);
   }
-
 </script>
 
-<Button on:click={loadCourses}>
-  (Re)load courses...
-</Button>
+<Button on:click={loadCourses}>(Re)load courses...</Button>
 <h2>Link a Google Course to {aspenCourse.title}</h2>
 <MenuList>
   {#each courses as course}
-    <li>      
-      <Button on:click={() => courseMap.setKey(aspenCourse.sourcedId, course.id)}>{course.name}</Button>      
+    <li>
+      <Button
+        on:click={() => {
+          courseMap.setKey(aspenCourse.sourcedId, course.id);
+          onLink(course, aspenCourse.sourcedId);
+        }}>{course.name}</Button
+      >
     </li>
   {/each}
 </MenuList>
 
 <style>
-li {
-  display: flex;
-  width: 800px;
-  justify-content: space-between;
-}
-li span {
-  width: 675px;
-}
+  li {
+    display: flex;
+    width: 800px;
+    justify-content: space-between;
+  }
+  li span {
+    width: 675px;
+  }
 </style>
